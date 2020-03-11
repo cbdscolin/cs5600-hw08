068925d291862de85cc3e3973a2c6c4adb57ebdf
Colin <cbdscolin@gmail.com>
2020-03-11 00:57:04 -0400

    Adding bucket allocator to opt_malloc

--- starter-hw08/opt_malloc.c
+++ starter-hw08/opt_malloc.c
@@ -8,27 +8,26 @@
 #include "xmalloc.h"
 #include <sys/mman.h>
 #include <pthread.h>
-//#include <stdio.h>
-
+#include <stdio.h>
+#include <assert.h>
+#include <error.h>
+#include <errno.h>
 // Memory allocator by Kernighan and Ritchie,
 // The C programming Language, 2nd ed.  Section 8.7.
 //
 // Then copied from xv6.
+typedef unsigned long uint;
 
 // TODO: Remove this stuff
-typedef unsigned long uint;
 
 /*
 static char* sbrk(uint nn) { return 0; }
 */
 // TODO: end of stuff to remove
 
-static pthread_mutex_t lock[105];
-static int lockAcq[105];
+static pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
+static int lockAcq = 0;
 
-static int pthreadInitComplete = 0;
-
-static pthread_mutex_t singleLock = PTHREAD_MUTEX_INITIALIZER;
 
 typedef long Align;
 
@@ -44,29 +43,36 @@ typedef union header Header;
 
 // TODO: This is shared global data.
 // You're going to want a mutex to protect this.
-static Header base[105];
-static Header *freep[105];
+static Header base;
+static Header *freep;
 
-int getThreadIndex() {
-   long tid = pthread_self();
-   int modulus = tid % 105;
-   //printf("tid: %ld  mod: %d\n", tid, modulus);
-   return modulus;
-}
+struct memchunk {
+    int totalsize;
+    int currentsize;
+    char *c;
+    struct memchunk *next;
+};
+
+struct memchunk *chunk32head[8];
+struct memchunk *chunk32tail[8];
+
+int limits[9] = {0, 8, 24, 72, 216, 648, 1944, 4096, 122880};
 
 void
 xfree(void *ap)
 {
+  //printf("Free called %p\n", ap);
+  if(1 == 1) {
+    return;
+  }   
   Header *bp, *p;
-  //printf("mutex locking\n");
-  int threadIndex = 0;
-  threadIndex = getThreadIndex();
-  if (lockAcq[threadIndex] == 0)
-    pthread_mutex_lock(&lock[threadIndex]);
-  //printf("mutex unlocked\n");  
+  if (lockAcq == 0) {
+    if(pthread_mutex_lock(&lock) < 0)
+        return;
+  }
 
   bp = (Header*)ap - 1;
-  for(p = freep[threadIndex]; !(bp > p && bp < p->s.ptr); p = p->s.ptr)
+  for(p = freep; !(bp > p && bp < p->s.ptr); p = p->s.ptr)
     if(p >= p->s.ptr && (bp > p || bp < p->s.ptr))
       break;
   if(bp + bp->s.size == p->s.ptr){
@@ -79,15 +85,15 @@ xfree(void *ap)
     p->s.ptr = bp->s.ptr;
   } else
     p->s.ptr = bp;
-  freep[threadIndex] = p;
-  if (lockAcq[threadIndex] == 0)
-    pthread_mutex_unlock(&lock[threadIndex]);
-  //printf("mutex released\n");
+  freep = p;
+  if (lockAcq == 0)
+    if(pthread_mutex_unlock(&lock) < 0)
+       return;
 }
 
 static Header*
 morecore(uint nu)
-{
+{   
   char *p;
   Header *hp;
   if(nu < 4096)
@@ -98,47 +104,102 @@ morecore(uint nu)
   if(p == (char*)-1) {
     return 0;
   }
-  int threadIndex = 0;
-  threadIndex = getThreadIndex();
   hp = (Header*)p;
   hp->s.size = nu;
-  lockAcq[threadIndex] = 1;
+  lockAcq = 1;
   xfree((void*)(hp + 1));
-  lockAcq[threadIndex] = 0;
-  return freep[threadIndex];
+  lockAcq = 0;
+  return freep;
 }
 
+
+int indexForMemory(uint bytes) {
+    if(bytes > 4096) {
+        return 8;
+    }
+    
+    for(int i = 0; i < 8; i++) {
+        if(i == 7) {
+            return 7;
+        }
+        if(bytes > limits[i] && bytes <= limits[i+1]) {
+            return i;        
+        }
+    }
+    return -1;
+}
 
 void*
 xmalloc(uint nbytes)
 {
   Header *p, *prevp;
   uint nunits;
-
-  if(pthreadInitComplete == 0) {
-    pthread_mutex_lock(&singleLock);
-    if(pthreadInitComplete == 0 ) {
-        for(int j = 0; j < 105; j++) {
-            pthread_mutex_init(&lock[j], NULL);
-            lockAcq[j] = 0; 
-        }
-        pthreadInitComplete = 1;
-    }
-    pthread_mutex_unlock(&singleLock);
-  }
-
-  int threadIndex = 0;
-  threadIndex = getThreadIndex();
-  
-  nunits = (nbytes + sizeof(Header) - 1)/sizeof(Header) + 1;
  
-  //printf("xmalloc locking\n");
-  pthread_mutex_lock(&lock[threadIndex]);
-  //printf("xmalloc unlcoked\n");
+  int bucketNo = indexForMemory(nbytes); 
 
-  if((prevp = freep[threadIndex]) == 0) {
-    base[threadIndex].s.ptr = freep[threadIndex] = prevp = &base[threadIndex];
-    base[threadIndex].s.size = 0;
+  if(bucketNo >= 0) {
+    if(pthread_mutex_lock(&lock) < 0) {
+        printf("pthread_lock failed\n");
+        assert(0 == 1);
+    };
+
+    long roundedMemory = limits[bucketNo + 1];
+    //printf("bytes: %ld, rounded: %ld\n", nbytes, roundedMemory);
+    struct memchunk *top, *oldPtr = 0;
+    long bytesToAllocate = 6553600;
+    if(roundedMemory > 40961) {
+      bytesToAllocate = nbytes;
+    }
+
+    top = chunk32tail[bucketNo];
+    while(top == 0 || top->currentsize + roundedMemory >= top->totalsize) {
+    if(top == 0) {
+        top = mmap(0, sizeof(struct memchunk), PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1 , 0);
+        if(MAP_FAILED == top) {
+            printf("Something failed code: %d\n", errno);
+            perror("mmap failed\n");
+            assert(0 == 1);
+        }
+
+        top->totalsize = bytesToAllocate;
+        top->currentsize = 0;
+        char *cl;
+        cl = mmap(0, bytesToAllocate, PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
+        if(cl == MAP_FAILED) {
+            printf("Something failed code: %d\n", errno);
+            perror("mmap failed\n");
+            assert(0 == 1);
+        }
+        top->c = cl;
+        if(oldPtr != 0) {
+            oldPtr->next = top;
+            chunk32tail[bucketNo] = top;
+        }
+    } else {
+        oldPtr = top;
+        top = top->next;
+    }
+    }
+    if(chunk32head[bucketNo] == 0) {
+        chunk32head[bucketNo] = top;
+        chunk32tail[bucketNo] = top;
+    }
+    char *retAddress = top->c + top->currentsize;
+    top->currentsize += roundedMemory;
+    if(pthread_mutex_unlock(&lock) < 0) {
+        printf("pthread_unlock failed\n");
+        assert(0 == 1);
+    };
+
+    return (void*) retAddress;
+  } 
+  printf("high mem  bucket: %d  nbytes %ld\n", bucketNo, nbytes); 
+  nunits = (nbytes + sizeof(Header) - 1)/sizeof(Header) + 1;
+
+
+  if((prevp = freep) == 0) {
+    base.s.ptr = freep = prevp = &base;
+    base.s.size = 0;
   }
   for(p = prevp->s.ptr; ; prevp = p, p = p->s.ptr){
     //printf("add: %p ,size: %ld , bytes: %ld\n", p->s.ptr, p->s.size, nbytes);
@@ -150,28 +211,35 @@ xmalloc(uint nbytes)
         p += p->s.size;
         p->s.size = nunits;
       }
-      freep[threadIndex] = prevp;
-      pthread_mutex_unlock(&lock[threadIndex]);
+      freep = prevp;
+      if(pthread_mutex_unlock(&lock) < 0) {
+        return 0;
+      }
      // printf("xmalloc freed nom\n");
       return (void*)(p + 1);
     }
-    if(p == freep[threadIndex])
+    if(p == freep)
       if((p = morecore(nunits)) == 0) {
        // printf("xmalloc ret err\n");
-        pthread_mutex_unlock(&lock[threadIndex]);
+        if(pthread_mutex_unlock(&lock) < 0) {
+            return 0;
+        }
         return 0;
       }
   }
   //printf("xmalloc end return\n");
-  pthread_mutex_unlock(&lock[threadIndex]);
+  if(pthread_mutex_unlock(&lock) < 0) {
+    return 0;
+  }
 }
 
 void*
-xrealloc(void* prev, size_t nn)
+xrealloc(void* prev, uint nn)
 {
   // TODO: Actually build realloc.
   char *prevPtr = (char *) prev;
   char *newPtr = xmalloc(nn);
+
   for(long i = 0; i < nn; i++) {
      newPtr[i] = prevPtr[i];
   }
