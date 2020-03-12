c9593a94e7bf64c7db9c1715e76894def283598d
Colin <cbdscolin@gmail.com>
2020-03-11 22:08:43 -0400

    Basic code cleanup - has only buckets

--- starter-hw08/opt_malloc.c
+++ starter-hw08/opt_malloc.c
@@ -1,10 +1,3 @@
-/*
-#include "types.h"
-#include "stat.h"
-#include "user.h"
-#include "param.h"
-*/
-
 #include "xmalloc.h"
 #include <sys/mman.h>
 #include <pthread.h>
@@ -12,39 +5,10 @@
 #include <assert.h>
 #include <error.h>
 #include <errno.h>
-// Memory allocator by Kernighan and Ritchie,
-// The C programming Language, 2nd ed.  Section 8.7.
-//
-// Then copied from xv6.
 typedef unsigned long uint;
-
-// TODO: Remove this stuff
-
-/*
-static char* sbrk(uint nn) { return 0; }
-*/
-// TODO: end of stuff to remove
 
 static pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
 static int lockAcq = 0;
-
-
-typedef long Align;
-
-union header {
-  struct {
-    union header *ptr;
-    uint size;
-  } s;
-  Align x;
-};
-
-typedef union header Header;
-
-// TODO: This is shared global data.
-// You're going to want a mutex to protect this.
-static Header base;
-static Header *freep;
 
 struct memchunk {
     int totalsize;
@@ -62,54 +26,10 @@ void
 xfree(void *ap)
 {
   //printf("Free called %p\n", ap);
-  if(1 == 1) {
+  lockAcq = 0;
+  if(0 == 0) {
     return;
   }   
-  Header *bp, *p;
-  if (lockAcq == 0) {
-    if(pthread_mutex_lock(&lock) < 0)
-        return;
-  }
-
-  bp = (Header*)ap - 1;
-  for(p = freep; !(bp > p && bp < p->s.ptr); p = p->s.ptr)
-    if(p >= p->s.ptr && (bp > p || bp < p->s.ptr))
-      break;
-  if(bp + bp->s.size == p->s.ptr){
-    bp->s.size += p->s.ptr->s.size;
-    bp->s.ptr = p->s.ptr->s.ptr;
-  } else
-    bp->s.ptr = p->s.ptr;
-  if(p + p->s.size == bp){
-    p->s.size += bp->s.size;
-    p->s.ptr = bp->s.ptr;
-  } else
-    p->s.ptr = bp;
-  freep = p;
-  if (lockAcq == 0)
-    if(pthread_mutex_unlock(&lock) < 0)
-       return;
-}
-
-static Header*
-morecore(uint nu)
-{   
-  char *p;
-  Header *hp;
-  if(nu < 4096)
-    nu = 4096;
-  // TODO: Replace sbrk use with mmap
-  p = mmap(0, nu * sizeof(Header), PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1 , 0); 
-
-  if(p == (char*)-1) {
-    return 0;
-  }
-  hp = (Header*)p;
-  hp->s.size = nu;
-  lockAcq = 1;
-  xfree((void*)(hp + 1));
-  lockAcq = 0;
-  return freep;
 }
 
 
@@ -132,12 +52,9 @@ int indexForMemory(uint bytes) {
 void*
 xmalloc(uint nbytes)
 {
-  Header *p, *prevp;
-  uint nunits;
  
-  int bucketNo = indexForMemory(nbytes); 
+    int bucketNo = indexForMemory(nbytes); 
 
-  if(bucketNo >= 0) {
     if(pthread_mutex_lock(&lock) < 0) {
         printf("pthread_lock failed\n");
         assert(0 == 1);
@@ -192,45 +109,8 @@ xmalloc(uint nbytes)
     };
 
     return (void*) retAddress;
-  } 
-  printf("high mem  bucket: %d  nbytes %ld\n", bucketNo, nbytes); 
-  nunits = (nbytes + sizeof(Header) - 1)/sizeof(Header) + 1;
 
-
-  if((prevp = freep) == 0) {
-    base.s.ptr = freep = prevp = &base;
-    base.s.size = 0;
-  }
-  for(p = prevp->s.ptr; ; prevp = p, p = p->s.ptr){
-    //printf("add: %p ,size: %ld , bytes: %ld\n", p->s.ptr, p->s.size, nbytes);
-    if(p->s.size >= nunits){
-      if(p->s.size == nunits)
-        prevp->s.ptr = p->s.ptr;
-      else {
-        p->s.size -= nunits;
-        p += p->s.size;
-        p->s.size = nunits;
-      }
-      freep = prevp;
-      if(pthread_mutex_unlock(&lock) < 0) {
-        return 0;
-      }
-     // printf("xmalloc freed nom\n");
-      return (void*)(p + 1);
-    }
-    if(p == freep)
-      if((p = morecore(nunits)) == 0) {
-       // printf("xmalloc ret err\n");
-        if(pthread_mutex_unlock(&lock) < 0) {
-            return 0;
-        }
-        return 0;
-      }
-  }
-  //printf("xmalloc end return\n");
-  if(pthread_mutex_unlock(&lock) < 0) {
-    return 0;
-  }
+  //printf("high mem  bucket: %d  nbytes %ld\n", bucketNo, nbytes); 
 }
 
 void*
